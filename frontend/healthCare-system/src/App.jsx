2d205963e96f2d056cf3f1bf827ee75a32df831e
Ali-Sarim <alisarim5437@gmail.com>
2026-02-15 17:58:45 +0530

    starting with project

---
+++ frontend/healthCare-system/src/App.jsx
@@ -0,0 +1,16 @@
+
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold text-center text-blue-600">HealthCare System</h1>
+    </>
+  )
+}
+
+export default App
